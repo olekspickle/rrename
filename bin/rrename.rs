b3c73424801027a780044bd68659b8a4edd9d2ff
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-05-16 15:23:08 +0200

    do nothing by default as most destructive tools do

--- bin/rrename.rs
+++ bin/rrename.rs
@@ -2,6 +2,12 @@ use clap::Parser;
 use rrename::Rrename;
 
 fn main() -> anyhow::Result<()> {
+    if std::env::args().len() == 1 {
+        // only binary name was provided
+        println!("rrename does nothing by default.\n\nTo see all options run: rrename --help");
+        return Ok(());
+    }
+
     let cli = Rrename::parse();
     cli.run()?;
 
